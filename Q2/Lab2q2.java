dbda906b48a02ae4cb24830607f4e85eef0c6b4a
IT24200716 <It24200716@my.sliit.lk>
2026-09-24 21:40:37 +0530

    Lab2Q2

---
+++ Q2/Lab2q2.java
@@ -0,0 +1,12 @@
+public class Lab2q2 {
+    public static void main(String[] args) {
+
+        double side = 10;
+        double squarePerimeter = 4 * side;
+
+        double pi = 22.0 / 7.0;
+        double radius = squarePerimeter / (2 * pi);
+
+        System.out.println("Radius = " + radius);
+    }
+}
