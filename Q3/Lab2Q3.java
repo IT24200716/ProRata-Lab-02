6a74ab2682170d9c4d4e942fdb7cbd79f7cd3a00
IT24200716 <It24200716@my.sliit.lk>
2026-09-24 21:41:06 +0530

    Lab2Q3

---
+++ Q3/Lab2Q3.java
@@ -0,0 +1,13 @@
+public class Lab2Q3{
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse = Math.sqrt(
+            (sideA * sideA) + (sideB * sideB)
+        );
+
+        System.out.println("Hypotenuse = " + hypotenuse);
+    }
+}
