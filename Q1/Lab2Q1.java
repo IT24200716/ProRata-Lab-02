c98071a92218d896ec8c36e7ca1cd805d3d481fd
IT24200716 <It24200716@my.sliit.lk>
2026-09-24 21:40:01 +0530

    Lab2Q1

---
+++ Q1/Lab2Q1.java
@@ -0,0 +1,12 @@
+public class Lab2Q1 {
+	public static void main(String args[]){
+
+
+		double perimeter=100;
+		double length=perimeter/3.5;
+		double width=3/4*length;
+
+		System.out.println("Length ="+length);
+		System.out.println("Width ="+width);
+       }
+}
